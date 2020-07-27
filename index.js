1a43ceee7720a60aedad3945388a02b21c5f1881
Sam Evans <evansst@icloud.com>
2020-07-27 09:57:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// function writeCards(namesArray, eventName) {
+//   for(let i = 0; i < namesArray.length; i++) {
+//     namesArray[i] = (`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+//   }
+//   return namesArray;
+// }
+
+function writeCards(namesArray, eventName) {
+  return namesArray.map(name => {
+    return `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+  });
+}
+
+function countDown(number) {
+  while (number >= 0 ) {
+    console.log(number);
+    number--;
+  }
+}
