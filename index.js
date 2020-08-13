74c631e80475f364a21cda7de8e4bc5b3c9c025d
Derek Turner <turner.derek.b@gmail.com>
2020-08-13 12:40:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+  const thankYouArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+     thankYouArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  };
+  return thankYouArray;
+};
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  };
+};
